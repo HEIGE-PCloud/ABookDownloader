529e5a8b2cec9b4f8f77f14e797c83a7f95ed76d
HEIGE-PCloud <heige.pcloud@outlook.com>
2020-12-04 14:35:00 +0000

    add preview button

--- src/FileListWidget.py
+++ src/FileListWidget.py
@@ -1,20 +1,27 @@
 import os
 import sys
-from PySide2.QtGui import QStandardItem, QStandardItemModel
-from PySide2.QtWidgets import QAbstractItemView, QApplication, QFileDialog, QFileSystemModel, QGridLayout, QLineEdit, QListView, QPushButton, QTreeView, QWidget
-import subprocess
-from PySide2.QtCore import QDir, Qt
+from PySide2.QtGui import QDesktopServices, QStandardItem, QStandardItemModel
+from PySide2.QtWidgets import QAbstractItemView, QApplication, QGridLayout, QListView, QPushButton, QWidget
+from PySide2.QtCore import QModelIndex, Qt
 class FileListWidget(QWidget):
 
     def __init__(self) -> None:
         QWidget.__init__(self)
+        self.index = None
         file_view = QListView()
         self.model = QStandardItemModel()
         file_view.setModel(self.model)
         file_view.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        file_view.clicked.connect(self.onClicked)
         file_view.doubleClicked.connect(self.onDoubleClicked)
+        open_file_button = QPushButton('Open')
+        open_file_button.clicked.connect(self.onOpenFile)
+        preview_file_button = QPushButton('Preview')
+        preview_file_button.clicked.connect(self.onPreviewFile)
         layout = QGridLayout()
-        layout.addWidget(file_view)
+        layout.addWidget(file_view, 0, 0, 1, 2)
+        layout.addWidget(open_file_button, 1, 0, 1, 1)
+        layout.addWidget(preview_file_button, 1, 1, 1, 1)
         layout.setMargin(0)
         self.setLayout(layout)
         
@@ -24,9 +31,27 @@ class FileListWidget(QWidget):
     def appendRow(self, item: QStandardItem):
         self.model.appendRow(item)
 
-    def onDoubleClicked(self, index):
-        item = self.model.itemFromIndex(index)
-        print(item.data(-1))
+    def onDoubleClicked(self, index: QModelIndex):
+        self.index = index
+        item = self.model.itemFromIndex(self.index)
+        url = item.data(-1)
+        QDesktopServices.openUrl(url)
+
+    def onClicked(self, index: QModelIndex):
+        self.index = index
+
+    def onOpenFile(self):
+        if self.index != None:
+            item = self.model.itemFromIndex(self.index)
+            url = item.data(-1)
+            QDesktopServices.openUrl(url)
+
+    def onPreviewFile(self):
+        if self.index != None:
+            item = self.model.itemFromIndex(self.index)
+            url = 'http://view.officeapps.live.com/op/view.aspx?src=' + item.data(-1)
+            print(url)
+            QDesktopServices.openUrl(url)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
