32648a2f92e33d36bb14fe0d8e091f3caddfe54c
HEIGE-PCloud <heige.pcloud@outlook.com>
2020-12-11 20:07:28 +0000

    Add copy and open log file button

--- src/ErrorMessageBox.py
+++ src/ErrorMessageBox.py
@@ -1,5 +1,8 @@
-from PySide2.QtWidgets import QGridLayout, QTextEdit, QWidget
+import os
+import subprocess
+from PySide2.QtWidgets import QGridLayout, QPushButton, QTextEdit, QWidget
 from PySide2.QtCore import Qt
+import pyperclip
 
 class ErrorMessageBox(QWidget):
 
@@ -7,8 +10,15 @@ class ErrorMessageBox(QWidget):
         super().__init__(parent)
         self.error = ''
         self.errorInformationBox = QTextEdit()
+        self.errorInformationBox.setReadOnly(True)
+        openFileButton = QPushButton('Open Log File')
+        openFileButton.clicked.connect(self.open)
+        copyButton = QPushButton('Copy')
+        copyButton.clicked.connect(self.copy)
         layout = QGridLayout()
-        layout.addWidget(self.errorInformationBox)
+        layout.addWidget(self.errorInformationBox, 0, 0, 1, 2)
+        layout.addWidget(openFileButton, 1, 0, 1, 1)
+        layout.addWidget(copyButton, 1, 1, 1, 1)
         layout.setMargin(0)
         self.setLayout(layout)
         self.setWindowFlag(Qt.WindowStaysOnTopHint)
@@ -22,3 +32,9 @@ class ErrorMessageBox(QWidget):
         self.errorInformationBox.setText(self.error)
         self.errorInformationBox.verticalScrollBar().setValue(self.errorInformationBox.verticalScrollBar().maximum())
         self.show()
+
+    def copy(self):
+        pyperclip.copy(self.errorInformationBox.toPlainText())
+
+    def open(self):
+        subprocess.run(['explorer', '.\\temp\\error.log'])
